7a402cb1683674b37636cd6bfb90550b8063731c
Garnett05 <charlleslimamota10@hotmail.com>
2020-03-08 22:28:11 -0300

    Terceira alternativa de solução. Expressão lambda atribuída a uma variável do tipo delegate

--- ComparisonT/Program.cs
+++ ComparisonT/Program.cs
@@ -14,7 +14,7 @@ namespace ComparisonT
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            Comparison<Product> comp = CompareProducts;
+            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
 
             list.Sort(comp);
 
@@ -23,10 +23,10 @@ namespace ComparisonT
                 Console.WriteLine(p);
             }
 
-            static int CompareProducts(Product p1, Product p2)
+            /*static int CompareProducts(Product p1, Product p2)
             {
                 return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-            }
+            }*/
         }
     }
 }
