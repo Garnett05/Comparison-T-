a6a40b1afab80963306e56ed2e091d099c825915
Garnett05 <charlleslimamota10@hotmail.com>
2020-03-08 22:24:52 -0300

    Segunda alternativa de solução. Referência de método atribuído a uma variável do tipo delegate

--- ComparisonT/Program.cs
+++ ComparisonT/Program.cs
@@ -14,9 +14,9 @@ namespace ComparisonT
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            //Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+            Comparison<Product> comp = CompareProducts;
 
-            list.Sort(CompareProducts);
+            list.Sort(comp);
 
             foreach (Product p in list)
             {
